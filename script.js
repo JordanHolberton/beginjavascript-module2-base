957fc244608498cbd51831605565e948cdc8de89
JordanHolberton <jordan.chatillon76@laposte.net>
2025-01-20 08:51:08 +0100

    feat(script.js): enhance logging with multiple console methods

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 // ℹ️ La propriété console.log() affiche un message dans la console Web.
 // ℹ️ Elle prend en paramètre une string (on verra plus tard ce que c'est) et l'affiche dans la console.
 // 🦁 Ta mission est d'afficher : Hello World!
+console.log("Hello World!");
+console.error("Hello World!");
+console.warn("Hello World!");
+console.info("Hello World!");
+console.debug("Hello World!");
+console.trace("Hello World!");
+console.dir("Hello World!");
+console.group("Hello World!");
+console.table("Hello World!");
+console.time("Hello World!");
